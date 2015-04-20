b10b1460eb5a3efd9740482e53ffacc4585e07b0
ThomasHedegaard <thomashj93@gmail.com>
2015-04-20 13:31:06 +0200

    init setup. Commit by: Thomas.

--- src/Main/Main.ino
+++ src/Main/Main.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+// Variables
+int Channel3 = 12;
+int throttle = 0;
 
+void setup() {
+	pinMode(Channel3, INPUT);
+	Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+	listenForController();
+	
+}
+
+// Will listen for signals from the transmitter
+// to control the drone
+void listenForController(){
+	throttle = map(pulseIn(Channel3, HIGH), 1005, 1980, 0, 179);
+
+	Serial.println(throttle);
+
+
+
+	// ------
+	Serial.println("---------------------");	
+
+	delay(1000);
 }
